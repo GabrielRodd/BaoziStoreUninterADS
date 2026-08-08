10c827d98a278e733697765f39d3c46d61f6bb27
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-08-08 19:27:42 -0300

    Initial commit - BaoziStore Spring Boot API REST

---
+++ src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/BaoziStoreUninterAdsApplication.java
@@ -0,0 +1,13 @@
+package dev.gabrielrodd.BaoziStoreUninterADS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoziStoreUninterAdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoziStoreUninterAdsApplication.class, args);
+	}
+
+}
