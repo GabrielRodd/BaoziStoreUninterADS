56de143a760b105e896218d60312299590e575c4
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-08-11 20:40:45 -0300

    feat: adiciona rotas GET e POST  no ClienteController e atualiza ClienteService

--- src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/cliente/ClienteController.java
+++ src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/cliente/ClienteController.java
@@ -1,4 +1,53 @@
 package dev.gabrielrodd.BaoziStoreUninterADS.cliente;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    //Injetando service
+    private ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    //GET
+    @GetMapping()
+    public ResponseEntity<List<ClienteModel>> mostrar() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(clienteService.mostrar());
+    }
+
+    //GET POR ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> mostrarPorId(@PathVariable Long id) {
+        ClienteModel clienteMostrar = clienteService.mostrarPorId(id);
+        if (clienteMostrar != null) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(clienteMostrar);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Cliente nao existente");
+        }
+    }
+
+    //POST
+    @PostMapping()
+    public ResponseEntity<String> criar(@RequestBody ClienteModel novoCliente) {
+        clienteService.criar(novoCliente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Cliente " + novoCliente.getNome() + " criado com sucesso");
+    }
+
 }
--- src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/cliente/ClienteService.java
+++ src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/cliente/ClienteService.java
@@ -1,4 +1,50 @@
 package dev.gabrielrodd.BaoziStoreUninterADS.cliente;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    //Injetando repository
+    private ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<ClienteModel> mostrar() {
+        return clienteRepository.findAll();
+    }
+
+    public ClienteModel mostrarPorId(Long id) {
+        Optional<ClienteModel> clienteBuscado = clienteRepository.findById(id);
+        return clienteBuscado.orElse(null);
+    }
+
+    public ClienteModel criar(ClienteModel novoCliente) {
+        return clienteRepository.save(novoCliente);
+    }
+
+    public ClienteModel deletar(Long id) {
+        ClienteModel clienteDeletar = mostrarPorId(id);
+        if (clienteDeletar != null) {
+            clienteRepository.deleteById(id);
+            return clienteDeletar;
+        } else {
+            return null;
+        }
+    }
+
+    public ClienteModel editar(Long id, ClienteModel clienteEditado) {
+        if (clienteRepository.existsById(id)) {
+            clienteEditado.setId(id);
+            return clienteRepository.save(clienteEditado);
+        }else{
+            return null;
+        }
+    }
+
 }
