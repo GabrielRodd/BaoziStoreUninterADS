218e0b65946b13b14e149cb0ea87e8b48f5a76cb
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-08-11 00:45:44 -0300

    feat: implementa endpoint GET /produto/mostrar no ProdutoController e ProdutoService

--- src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/produto/ProdutoController.java
+++ src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/produto/ProdutoController.java
@@ -1,4 +1,28 @@
 package dev.gabrielrodd.BaoziStoreUninterADS.produto;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
+
+    //Injetando dependencia service
+    private ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping("/mostrar")
+    public ResponseEntity<List<ProdutoModel>> mostrar() {
+        List<ProdutoModel> listaProdutos = produtoService.mostrar();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(listaProdutos);
+    }
 }
--- src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/produto/ProdutoService.java
+++ src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/produto/ProdutoService.java
@@ -1,4 +1,22 @@
 package dev.gabrielrodd.BaoziStoreUninterADS.produto;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    //Injetando dependencia do repository
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoService (ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<ProdutoModel> mostrar() {
+        return produtoRepository.findAll();
+    }
+
+
 }
