07e2d4cd08b3e094965bc8bf492161027f1e96b5
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-08-13 12:20:43 -0300

    feat: Adiciona as rotas e metodos para Pedidos

--- src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/pedido/PedidoController.java
+++ src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/pedido/PedidoController.java
@@ -1,4 +1,54 @@
 package dev.gabrielrodd.BaoziStoreUninterADS.pedido;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
+
+    //Injetando Service
+    private PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService  = pedidoService;
+    }
+
+    public ResponseEntity<List<PedidoModel>> mostrar() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(pedidoService.mostrar());
+    }
+
+    public ResponseEntity<Object> mostrarPorId(@PathVariable Long id) {
+        PedidoModel pedidoMostrar = pedidoService.mostrarPorId(id);
+        if (pedidoMostrar != null) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(pedidoMostrar);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Pedido de id " + id + " nao existe");
+        }
+    }
+
+    public ResponseEntity<PedidoModel> criar(@RequestBody PedidoModel novoPedido) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(pedidoService.criar(novoPedido));
+    }
+
+    public ResponseEntity<String> deletar(@PathVariable Long id) {
+        Boolean deletarEncontrado = pedidoService.deletar(id);
+        if (deletarEncontrado == true) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Pedido de ID " + id + " deletado com sucesso");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Pedido de ID " + id + " nao existe");
+        }
+    }
 }
--- src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/pedido/PedidoService.java
+++ src/main/java/dev/gabrielrodd/BaoziStoreUninterADS/pedido/PedidoService.java
@@ -1,4 +1,49 @@
 package dev.gabrielrodd.BaoziStoreUninterADS.pedido;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PedidoService {
+
+    //Injetando repository
+    private PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public List<PedidoModel> mostrar() {
+        return pedidoRepository.findAll();
+    }
+
+    public PedidoModel mostrarPorId(Long id) {
+        Optional<PedidoModel> pedidoMostrar = pedidoRepository.findById(id);
+        return pedidoMostrar.orElse(null);
+    }
+
+    public PedidoModel criar(PedidoModel novoPedido) {
+        return pedidoRepository.save(novoPedido);
+    }
+
+    public Boolean deletar(Long id) {
+        Boolean pedidoDeletar = pedidoRepository.existsById(id);
+        if (pedidoDeletar != false) {
+            pedidoRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public PedidoModel editar(Long id, PedidoModel pedidoEditado) {
+        if (pedidoRepository.existsById(id)) {
+            pedidoEditado.setId(id);
+            return pedidoRepository.save(pedidoEditado);
+        }else{
+            return null;
+        }
+    }
 }
